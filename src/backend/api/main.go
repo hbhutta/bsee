155fdd8079125ad1c1d1ade9213173a63fe8ca6e
hbhutta <haadbhutta@gmail.com>
2023-12-25 18:51:30 -0800

    Writing lambda function for AWS

--- src/backend/api/main.go
+++ src/backend/api/main.go
@@ -1,22 +1,24 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	"math/rand"
 	"os"
 
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/joho/godotenv"
-	_ "github.com/lib/pq" // Specifically for PostgreSQL
 )
 
+type MyEvent struct {
+	Name string `json:"name"`
+}
+
 type Attraction struct {
-	name    string // `json:"name"`
-	city    string // `json:"city"`
-	address string // `json:"address"`
+	Name    string `json:"name"`
+	City    string `json:"city"`
+	Address string `json:"address"`
 }
 
 var (
@@ -28,38 +30,28 @@ var (
 	connStr    string
 )
 
-func main() {
-	if err := godotenv.Load("../../../.env"); err != nil {
-		log.Fatal("Could not load environment variables with godotenv.")
+func HandleRequest(ctx context.Context, event *Attraction) (*string, error) {
+	// TODO
+	if event == nil {
+		return nil, fmt.Errorf("recieved nil event")
 	}
+	attraction := retrieveRandomAttraction()
+	message := fmt.Sprintf("%s %s %s", attraction.Name, attraction.City, attraction.Address)
+	return &message, nil
 
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
+
+func retrieveRandomAttraction() Attraction {
 	DBNAME = os.Getenv("DBNAME")
 	DBUSER = os.Getenv("DBUSER")
 	DBPASSWORD = os.Getenv("DBPASSWORD")
 	DBHOST = os.Getenv("DBHOST")
 	DBPORT = os.Getenv("DBPORT")
 
-	// connStr = fmt.Sprintf("dbname=%s user=%s password=%s host=%s port=%s sslmode=disable", DBNAME, DBUSER, DBPASSWORD, DBHOST, DBPORT)
-
-	// router := gin.Default()
-	// router.GET("/attraction", getRandomAttraction)
-	// router.Run("localhost:8080")
-	lambda.Start(handler)
-}
-
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	log.Println("In handler...")
-	attraction := retrieveRandomAttraction()
-	attraction_string := fmt.Sprintf("Name: %s, City: %s, Address: %s", attraction.name, attraction.city, attraction.address)
-	response := events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       attraction_string,
-	}
-
-	return response, nil
-}
-
-func retrieveRandomAttraction() Attraction {
 	connStr = fmt.Sprintf("dbname=%s user=%s password=%s host=%s port=%s sslmode=disable", DBNAME, DBUSER, DBPASSWORD, DBHOST, DBPORT)
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
@@ -88,46 +80,10 @@ func retrieveRandomAttraction() Attraction {
 		}
 
 		attraction = Attraction{
-			name:    name,
-			city:    city,
-			address: address,
+			Name:    name,
+			City:    city,
+			Address: address,
 		}
 	}
 	return attraction
 }
-
-// func getRandomAttraction(c *gin.Context) {
-// 	attraction := retrieveRandomAttraction()
-// 	fmt.Printf("Old Attraction Info: Name: %s | City: %s | Address: %s \n", attraction.name, attraction.city, attraction.address)
-
-// 	// Check if city matches address
-// 	// e.g)
-// 	// {
-// 	// 	"address": "333 Gladwin Rd, Abbotsford, BC V2S 8A7, Canada",
-// 	// 	"city": "Langley",
-// 	// 	"name": "Applebarn Pumpkin Farm (Taves Farms)"
-// 	// }
-// 	// address := attraction.address
-// 	// address_components := strings.Split(address, ", ")
-// 	// city := address_components[1]
-// 	// if city != address {
-// 	// 	_, updateErr := db.Exec("UPDATE bsee SET city = %s WHERE address = %s", city, address) //Exec is for read-only database migrations
-// 	// 	if updateErr != nil {
-// 	// 		log.Fatal("Update error")
-// 	// 		log.Fatal(updateErr)
-// 	// 	}
-// 	// 	c.JSON(http.StatusOK, gin.H{
-// 	// 		"name":    attraction.name,
-// 	// 		"city":    attraction.city,
-// 	// 		"address": attraction.address,
-// 	// 	})
-// 	// 	fmt.Printf("Updated. Responded with attraction: Name: %s | City: %s | Address: %s \n", attraction.name, city, attraction.address)
-// 	// }
-// 	c.JSON(http.StatusOK, gin.H{
-// 		"name":    attraction.name,
-// 		"city":    attraction.city,
-// 		"address": attraction.address,
-// 	})
-// 	fmt.Printf("Responded with attraction: Name: %s | City: %s | Address: %s \n", attraction.name, attraction.city, attraction.address)
-
-// }
